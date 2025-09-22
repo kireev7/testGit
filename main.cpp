6356e4860f5d8b28ef7fb31c6b58e689d1f5f0b2
SerKrv <kireevsergiy7@gmail.com>
2025-09-22 19:16:58 +0300

    add summary func

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
+int a = 18;
+int b = 20;
+
+int sum(int a, int b){
+    return a + b;
+}
+
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    std::cout << sum(a, b) << std::endl;
     return 0;
 }
